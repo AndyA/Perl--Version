8fe3d52becbc0e33414f230b7d931447c7bad1cc
Andy Armstrong <andy@hexten.net>
2009-03-07 16:41:35 +0000

    Version bump

--- lib/Perl/Version.pm
+++ lib/Perl/Version.pm
@@ -5,7 +5,7 @@ use strict;
 use Carp;
 use Scalar::Util qw( blessed );
 
-our $VERSION = '1.008';
+our $VERSION = '1.009';
 
 use overload (
   '""'  => \&stringify,
@@ -371,7 +371,7 @@ Perl::Version - Parse and manipulate Perl version strings
 
 =head1 VERSION
 
-This document describes Perl::Version version 1.008
+This document describes Perl::Version version 1.009
 
 =head1 SYNOPSIS
 
