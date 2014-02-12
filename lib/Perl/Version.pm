d34653b1883d2bd9c99bcc961ede60e2a47a6c79
brian d foy <brian.d.foy@gmail.com>
2014-02-12 15:55:19 -0500

    Get rid of module signatures

--- lib/Perl/Version.pm
+++ lib/Perl/Version.pm
@@ -5,7 +5,7 @@ use strict;
 use Carp;
 use Scalar::Util qw( blessed );
 
-our $VERSION = '1.012';
+our $VERSION = '1.013';
 
 use overload (
   '""'  => \&stringify,
@@ -371,7 +371,7 @@ Perl::Version - Parse and manipulate Perl version strings
 
 =head1 VERSION
 
-This document describes Perl::Version version 1.012
+This document describes Perl::Version version 1.013
 
 =head1 SYNOPSIS
 
